c31dcfa67fd3d93f2b6712824a5a07bb94cd091a
fbrzzhormazabal <lfabrop7@hotmail.com>
2022-11-03 13:19:20 -0300

    commit de ejemplo de conexion con mysql y npm

--- test.js
+++ test.js
@@ -11,3 +11,11 @@ let n = sum(10, 18);
 console.log(n);
 console.log("Hola mundo")
 //end of program
+
+
+//conexion 
+var Connection = require('database-js').Connection;
+
+var conn =	
+    //conexion con mysql. 
+    new Connection("mysql://user:password@localhost/test");
