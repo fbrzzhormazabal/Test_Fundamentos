fe504dc292c355925065dcb167b2730b5fd94096
fbrzzhormazabal <lfabrop7@hotmail.com>
2022-11-17 23:23:27 -0300

    Test encuesta (funciona)

--- aplicacion-de-test/src/ui/encuesta.js
+++ aplicacion-de-test/src/ui/encuesta.js
@@ -45,6 +45,8 @@ function resultado() {
         p1=0.5
     }else if(document.getElementById('q1_r3').checked==true) {       
         p1=0
+    }else{
+        console.error(error);
     }
     // 2a pregunta
     if (document.getElementById('q2_r1').checked==true) {
@@ -53,14 +55,18 @@ function resultado() {
         p2=0.5
     }else if(document.getElementById('q2_r3').checked==true) {       
         p2=0
+    }else{
+        console.error(error);
     }
     // 3a pregunt2
     if (document.getElementById('q3_r1').checked==true) {
         p3=1
     }else if(document.getElementById('q3_r2').checked==true){
-        p3=0.3
+        p3=0.5
     }else if(document.getElementById('q3_r3').checked==true) {       
         p3=0
+    }else{
+        console.error(error);
     }
     //4a pregunta 
     if (document.getElementById('q10_r1').checked==true) {
@@ -69,6 +75,8 @@ function resultado() {
         p10=0.5
     }else if(document.getElementById('q10_r3').checked==true) {       
         p10=0
+    }else{
+        console.error(error);
     }
     // 5a pregunta
     if (document.getElementById('q4_r1').checked==true) {
@@ -77,6 +85,8 @@ function resultado() {
         p4=0.5
     }else if(document.getElementById('q4_r3').checked==true) {       
         p4=0
+    }else{
+        console.error(error);
     }
     // 6a pregunta
     if (document.getElementById('q5_r1').checked==true) {
@@ -85,6 +95,8 @@ function resultado() {
         p5=0.5
     }else if(document.getElementById('q5_r3').checked==true) {       
         p5=0
+    }else{
+        console.error(error);
     }
     // 7a pregunta
     if (document.getElementById('q6_r1').checked==true) {
@@ -93,6 +105,8 @@ function resultado() {
         p6=0.5
     }else if(document.getElementById('q6_r3').checked==true) {       
         p6=0
+    }else{
+        console.error(error);
     }
     // 8a pregunta
     if (document.getElementById('q7_r1').checked==true) {
@@ -101,6 +115,8 @@ function resultado() {
         p7=0.5
     }else if(document.getElementById('q7_r3').checked==true) {       
         p7=0
+    }else{
+        console.error(error);
     }
     // 9a pregunta
     if (document.getElementById('q8_r1').checked==true) {
@@ -109,6 +125,8 @@ function resultado() {
         p8=0.5
     }else if(document.getElementById('q8_r3').checked==true) {       
         p8=0
+    }else{
+        console.error(error);
     }
     // 10a pregunta
     if (document.getElementById('q9_r1').checked==true) {
@@ -117,6 +135,8 @@ function resultado() {
         p9=0.5
     }else if(document.getElementById('q9_r3').checked==true) {       
         p9=0
+    }else{
+        console.error(error);
     }
 
     nota=p1+p2+p3+p4+p5+p6+p7+p8+p9+p10;
@@ -127,4 +147,5 @@ function resultado() {
     }
     alert(" Puntos totales :" + nota);
     window.location = 'encuesta.html'
+    location.href= 'index.html'
 }
